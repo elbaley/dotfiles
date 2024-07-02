3e417e25989afb031fffc80fe6423636a0671672
elbaley <lebafurkan@outlook.com>
2024-07-02 13:09:09 +0300

    fix: change max items in arena

--- .config/fvim/lua/furkan/plugins/arena.lua
+++ .config/fvim/lua/furkan/plugins/arena.lua
@@ -4,6 +4,8 @@ return {
   config = function()
     vim.keymap.set('n', '<leader><leader>', '<cmd>ArenaToggle<cr>', { desc = 'Arena toggle' })
 
-    require('arena').setup()
+    require('arena').setup {
+      max_items = 9,
+    }
   end,
 }
