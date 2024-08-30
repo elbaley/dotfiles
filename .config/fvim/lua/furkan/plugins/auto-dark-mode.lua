44d37462b6fa034f30260465b4f5bb6d1cde38dd
elbaley <lebafurkan@outlook.com>
2024-08-31 02:55:51 +0300

    fix: change default light theme

--- .config/fvim/lua/furkan/plugins/auto-dark-mode.lua
+++ .config/fvim/lua/furkan/plugins/auto-dark-mode.lua
@@ -11,7 +11,7 @@ return {
       end,
       set_light_mode = function()
         vim.api.nvim_set_option('background', 'light')
-        vim.cmd 'colorscheme catppuccin-latte'
+        vim.cmd 'colorscheme catppuccin'
       end,
     }
   end,
