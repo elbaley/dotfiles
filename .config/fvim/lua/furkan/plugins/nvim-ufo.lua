fa667a5e4197b627c66c5303e2da2597593f83c3
elbaley <lebafurkan@outlook.com>
2024-08-31 01:25:49 +0300

    fix: nvim ufo column fix

--- .config/fvim/lua/furkan/plugins/nvim-ufo.lua
+++ .config/fvim/lua/furkan/plugins/nvim-ufo.lua
@@ -3,7 +3,7 @@ return {
   event = 'BufReadPost',
   init = function()
     vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-    vim.o.foldcolumn = '1'
+    vim.o.foldcolumn = '0'
     vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
     vim.o.foldlevelstart = 99
     vim.o.foldenable = true
